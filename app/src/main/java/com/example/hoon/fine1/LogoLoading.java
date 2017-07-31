f4aaec3613ac17566985a855afcd13bd9aa343f2
Unknown <ljhjj9497@naver.com>
2017-07-31 13:33:19 +0900

    FIrst

---
+++ app/src/main/java/com/example/hoon/fine1/LogoLoading.java
@@ -0,0 +1,26 @@
+package com.example.hoon.fine1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class LogoLoading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_loading);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext() , MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
